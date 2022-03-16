9c580df4fe2015e5c17aee3959fe679b63602b25
David Araújo <david2araujo5@gmail.com>
2022-03-16 22:06:46 +0000

    Update

--- P/P01/ex1_07.java
+++ P/P01/ex1_07.java
@@ -1,24 +1,31 @@
-import java.util.Arrays;
-
 public class ex1_07 {
     public static void main(String[] args) {
-        Node root = new Node(args[0]);
-        String[] rem = Arrays.copyOfRange(args, 1, args.length - 1);
-        root = ExpressionTree(root, rem);
+        Node root = null;
+        root = ExpressionTree(root, args[0], null);
+        for (int i = 1; i < args.length; i += 2) {
+            root = ExpressionTree(root, args[i], args[i + 1]);
+        }
     }
 
-    private static Node ExpressionTree(Node current, String[] src) {
-        
+    private static Node ExpressionTree(Node current, String r, String l) {
+        if (current == null) {
+            return new Node(r);
+        }
+
+        // Don't know tbh
 
         return current;
     }
 }
 
-class Node<T> {
-    T value;
-    Node<T> right;
-    Node<T> left;
-    public Node(T value) {
+class Node {
+    String value;
+    Node right;
+    Node left;
+
+    public Node(String value) {
         this.value = value;
+        this.right = null;
+        this.left = null;
     }
 }
