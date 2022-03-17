e352267e962ea32dcea8a8a6b043b805f2714e4d
user66 <user66@localhost.localdomain>
2022-03-17 10:52:05 +0000

    Update

--- P/P01/ex1_07.java
+++ P/P01/ex1_07.java
@@ -1,24 +1,35 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class ex1_07 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(String.join(" ", arr));
         Node root = new Node(args[0]);
-        String[] rem = Arrays.copyOfRange(args, 1, args.length - 1);
-        root = ExpressionTree(root, rem);
+        root = ExpressionTree(root, sc);
     }
 
-    private static Node ExpressionTree(Node current, String[] src) {
-        
+    private static Node ExpressionTree(Node current, Scanner sc) {
+        if(current == null){
+            return new Node(sc.next());
+        }
+
+        if(sc.hasNext("[=*/+-]")){
+            current.left = ExpressionTree(current.left, sc);
+            current.right = ExpressionTree(current.right, sc);
+        }else if(sc.hasNext("[A-Za-a_][A-Za-z0-9_]*") || sc.hasNextDouble()){
+            current.left = ExpressionTree(current.left, sc);
+        }
 
         return current;
     }
 }
 
-class Node<T> {
-    T value;
-    Node<T> right;
-    Node<T> left;
-    public Node(T value) {
+class Node {
+    Object value;
+    Node right;
+    Node left;
+    public Node(Object value) {
         this.value = value;
+        right = null;
+        left = null;
     }
 }
