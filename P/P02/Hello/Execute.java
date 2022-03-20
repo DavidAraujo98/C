a1aa75dca3f1416723d6ffee2848792b813c441b
David Araújo <david2araujo5@gmail.com>
2022-03-20 22:54:39 +0000

    Update

---
+++ P/P02/Hello/Execute.java
@@ -0,0 +1,26 @@
+
+// Generated from Hello.g4 by ANTLR 4.9.3
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link HelloVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <String> The return type of the visit operation. Use {@link Void} for
+ *                 operations with no return type.
+ */
+public class Execute<String> extends AbstractParseTreeVisitor<String> implements HelloVisitor<String> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.
+	 * </p>
+	 */
+	@Override
+	public String visitGreetings(HelloParser.GreetingsContext ctx) {
+		return visitChildren(ctx);
+	}
+}
