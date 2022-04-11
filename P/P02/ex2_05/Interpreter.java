4dd5541ea690941a1abce192b51c448afe294ca8
David Araujo <david2araujo5@gmail.com>
2022-04-11 10:29:08 +0100

    ex2_05 w/ listener complete

--- P/P02/ex2_05/Interpreter.java
+++ P/P02/ex2_05/Interpreter.java
@@ -1,36 +1,56 @@
+import java.util.HashMap;
+
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Interpreter extends TranslatorBaseListener {
+   HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-   @Override public void enterProgram(TranslatorParser.ProgramContext ctx) {
+   @Override
+   public void enterProgram(TranslatorParser.ProgramContext ctx) {
+      ctx.stat();
    }
 
-   @Override public void exitProgram(TranslatorParser.ProgramContext ctx) {
+   @Override
+   public void exitProgram(TranslatorParser.ProgramContext ctx) {
    }
 
-   @Override public void enterStat(TranslatorParser.StatContext ctx) {
+   public HashMap<String, Integer> getValue() {
+      return map;
    }
 
-   @Override public void exitStat(TranslatorParser.StatContext ctx) {
+   @Override
+   public void enterStat(TranslatorParser.StatContext ctx) {
+      ctx.expr();
    }
 
-   @Override public void enterExpr(TranslatorParser.ExprContext ctx) {
+   @Override
+   public void exitStat(TranslatorParser.StatContext ctx) {
    }
 
-   @Override public void exitExpr(TranslatorParser.ExprContext ctx) {
+   @Override
+   public void enterExpr(TranslatorParser.ExprContext ctx) {
+      map.put(ctx.T().getText(), Integer.parseInt(ctx.I().getText()));
    }
 
-   @Override public void enterEveryRule(ParserRuleContext ctx) {
+   @Override
+   public void exitExpr(TranslatorParser.ExprContext ctx) {
    }
 
-   @Override public void exitEveryRule(ParserRuleContext ctx) {
+   @Override
+   public void enterEveryRule(ParserRuleContext ctx) {
    }
 
-   @Override public void visitTerminal(TerminalNode node) {
+   @Override
+   public void exitEveryRule(ParserRuleContext ctx) {
    }
 
-   @Override public void visitErrorNode(ErrorNode node) {
+   @Override
+   public void visitTerminal(TerminalNode node) {
+   }
+
+   @Override
+   public void visitErrorNode(ErrorNode node) {
    }
 }
